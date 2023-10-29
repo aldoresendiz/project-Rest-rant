eac3132678fc70e2d575d127db032a861f805c24
aldoresendiz <aldoresendiz@gmail.com>
2023-10-29 01:04:59 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,7 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 });
 
+  
 router.get('/', (req, res) => 
 {
     // res.send('GET /places');
@@ -24,6 +25,24 @@ router.get('/', (req, res) =>
     //     }]
     res.render('places/index', {places});
 });
+
+// SHOW
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id])
+    {
+        res.render('places/show')
+    }
+    else {
+        res.render('places/show', {place: places[id]})
+    }
+
+    // res.render('places/show')
+  })
+
 
 // CREATE
 router.post('/', (req, res) => {
